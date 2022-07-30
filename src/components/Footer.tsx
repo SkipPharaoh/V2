14b33f3d62ce25b53e506059cac76e213efc5050
skippharaoh <caniggiathompson@gmail.com>
2022-07-30 08:11:11 -0400

    Footer Component complete

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer: React.FC<{}> = () => {
+  return (
+    <div>
+        <footer>
+            <a href="/">
+                <img src="" alt="Logo" />
+            </a>
+
+            <p>Built & Designed by Caniggia Thompson</p>
+            <p>&copy; All rights reserved</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About';
+import Footer from './Footer';
 import Projects from './Projects'
 
 const Home: React.FC<{}> = () => {
@@ -8,6 +9,7 @@ const Home: React.FC<{}> = () => {
     <div>
         <About />
         <Projects />
+        <Footer />
     </div>
   )
 }
