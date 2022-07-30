f6d697c9d90a99e3fce3c8069e6cfbe29caf197e
skippharaoh <caniggiathompson@gmail.com>
2022-07-30 08:06:32 -0400

    Disc Component complete

---
+++ src/components/Disc.tsx
@@ -0,0 +1,15 @@
+const Disc: React.FC = () => {
+  return (
+    <div>
+      <object
+        data="/docs/Disc.pdf"
+        type="application/pdf"
+        width="100%"
+        height="1050"
+        aria-label="Disc"
+      ></object>
+    </div>
+  );
+};
+
+export default Disc;
