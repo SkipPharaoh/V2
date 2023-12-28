4ab272333ee1f4359e4dbeadd56812378723ba14
skippharaoh <caniggiathompson@gmail.com>
2023-12-28 13:16:22 -0500

    Added ProjectCard component

---
+++ public/Projects.ts
@@ -0,0 +1,68 @@
+import Aizen from "../public/assets/icons/aizen.svg";
+import Kamui from "../public/assets/icons/kamui.svg";
+import Pokedex from "../public/assets/icons/pokedex.svg";
+import Portfolio from "../public/assets/icons/portfolio.svg";
+import Evo from "../public/assets/icons/evo.png";
+import Nike from "../public/assets/icons/nike.png";
+import Nft from "../public/assets/icons/nft.png";
+
+// TODO: Update images to match the appropriate project
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Aizen,
+    title: "Sonya's Portfolio",
+    description: "Portfolio built with astro",
+    link: "/work/aizen",
+    target: "_self",
+  },
+  {
+    id: 2,
+    image: Kamui,
+    title: "Noir Tribe Docs",
+    description: "Design system for reusable components",
+    link: "/work/kamui",
+    target: "_self",
+  },
+  {
+    id: 3,
+    image: Evo,
+    title: "Noir Tech Tribe",
+    description: "Tech blog made using NextJS and Tailwind CSS",
+    link: "https://www.noirtechtribe.com/",
+    target: "_self",
+  },
+  {
+    id: 4,
+    image: Pokedex,
+    title: "Pokedex",
+    description: "Gotta Catch'em All. Explore your Pokemon",
+    link: "/work/pokedex",
+    target: "_blank",
+  },
+  {
+    id: 5,
+    image: Nike,
+    title: "Portfolio v1",
+    description: "1st iteration of this portfolio",
+    link: "https://www.caniggiathompson.com/",
+    target: "_blank",
+  },
+  {
+    id: 6,
+    image: Nft,
+    title: "Blackjack",
+    description: "Popular casino card game",
+    link: "https://skippharaoh.github.io/Project-Blackjack/",
+    target: "_blank",
+  },
+  {
+    id: 7,
+    image: Portfolio,
+    title: "Portfolio v2",
+    description: "Portfolio made using NextJS and Tailwind CSS",
+    link: "",
+    target: "",
+  },
+];
