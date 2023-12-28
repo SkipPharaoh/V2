f630a1ed627733ad42ece66aee621bffdff5ee2a
skippharaoh <caniggiathompson@gmail.com>
2023-12-28 15:48:58 -0500

    Added TechCard component

---
+++ src/components/TechCard.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { forwardRef } from "react";
+import Image from "next/image";
+
+interface TechCardProps {
+  id: number;
+  main: string;
+  alt: string;
+}
+
+const TechCard = forwardRef<HTMLDivElement, TechCardProps>(
+  ({ id, main, alt, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className="tech group relative dark:bg-white/25 lg:bg-none lg:dark:hover:bg-slate-50/50  h-10 w-10 sm:h-12 sm:w-12 md:h-16 md:w-16 lg:h-20 lg:w-20 tech rounded-md sm:rounded-lg md:rounded-xl "
+      >
+        <div className="h-full w-full p-2 md:p-3 lg:p-4 relative">
+          <Image
+            src={main}
+            fill
+            className="select-none group-hover:scale-[1.1] hover:cursor-pointer transition-all duration-700 object-contain p-2 md:p-3 rounded-lg md:rounded-xl lg:rounded-2xl"
+            alt={alt}
+            sizes="(max-width: 768px) 100vw,
+          (max-width: 1200px) 50vw,
+          33vw"
+          />
+        </div>
+      </div>
+    );
+  }
+);
+
+TechCard.displayName = "TechCard";
+
+export default TechCard;
