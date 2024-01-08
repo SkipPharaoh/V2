f09d9fd97efa17251b6b78d5d4a333cf3925090b
skippharaoh <caniggiathompson@gmail.com>
2024-01-08 16:40:31 -0500

    Decoupled WorkPage & ProjectCard code

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { forwardRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { forwardRef, useState } from "react";
 import { projectsData } from "../../public/Projects";
-import AlertModal from "./AlertModal";
+import ProjectCardAlertModal from "./ProjectCardAlertModal";
 
 interface ProjectCardProps {}
 
@@ -14,30 +14,6 @@ const ProjectCard = forwardRef<HTMLDivElement, ProjectCardProps>(
 
     const handleAlertClose = () => {
       setAlertVisible(false);
-    };
-
-    const ProjectCardAlertModal = () => {
-      return (
-        <AlertModal isVisible={isAlertVisible} onClose={handleAlertClose}>
-          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <h3
-                  className="text-base font-semibold leading-6 text-gray-900"
-                  id="modal-title"
-                >
-                  Hold up!
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    You are already on this site! 🤩
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </AlertModal>
-      );
     };
 
     return (
@@ -80,7 +56,10 @@ const ProjectCard = forwardRef<HTMLDivElement, ProjectCardProps>(
             </div>
           );
         })}
-        <ProjectCardAlertModal />
+        <ProjectCardAlertModal
+          isAlertVisible={isAlertVisible}
+          handleAlertClose={handleAlertClose}
+        />
       </div>
     );
   }
