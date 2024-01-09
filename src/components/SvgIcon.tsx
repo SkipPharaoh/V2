d8f5b19ea45938baae8e7d736bf19621b0c1739d
skippharaoh <caniggiathompson@gmail.com>
2024-01-09 12:12:34 -0500

    Added SVGs & EncodedPNGPaths to isolated files

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -1,14 +1,18 @@
 import React, { SVGProps, forwardRef } from "react";
 import { iconPaths } from "../../public/IconPaths";
+import { encodedPNGPaths } from "../../public/EncodedPNGPaths";
 
+type IconType = keyof typeof iconPaths | keyof typeof encodedPNGPaths;
 interface SvgIconProps extends SVGProps<SVGSVGElement> {
   // Add additional props specific to your SVG component
-  icon: keyof typeof iconPaths;
+  icon: IconType;
 }
 
 const SvgIcon = forwardRef<SVGSVGElement, SvgIconProps>(
   ({ icon, ...props }, ref) => {
-    const iconPath: string = iconPaths[icon];
+    const iconPath: string =
+      (iconPaths as Record<string, string>)[icon] ||
+      (encodedPNGPaths as Record<string, string>)[icon];
 
     return (
       <svg ref={ref} {...props}>
