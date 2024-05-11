a4674d10fbb5d129ed8b8c8602f53aa0f359085a
skippharaoh <caniggiathompson@gmail.com>
2024-05-11 19:03:03 -0400

    Refactored GitHubStats code

--- src/components/GitHubStats.tsx
+++ src/components/GitHubStats.tsx
@@ -2,23 +2,36 @@ import getGitHubData from "@/data-fetch/getGitHubData";
 import { FaRegStar } from "react-icons/fa";
 import { FiGitBranch } from "react-icons/fi";
 
+interface GitHubInfo {
+  icon: typeof FaRegStar | typeof FiGitBranch;
+  label: number;
+}
+
 const GitHubStats = async () => {
   const { star, forks } = await getGitHubData();
 
+  const gitHubInfo: GitHubInfo[] = [
+    { icon: FaRegStar, label: star },
+    { icon: FiGitBranch, label: forks },
+  ];
+
   return (
     <div className="flex space-x-4 dark:text-white text-black">
-      <div className="flex dark:group-hover:text-blue-400 group-hover:text-red-500  space-x-1 items-center ">
-        <FaRegStar className="h-4 w-4 group-hover:stroke-[2.2px] dark:group-hover:fill-blue-400  group-hover:fill-red-500 " />
-        <p className="font-semibold text-sm group-hover:font-extrabold ">
-          {star}
-        </p>
-      </div>
-      <div className="flex  dark:group-hover:text-blue-400 group-hover:text-red-500 space-x-1 items-center ">
-        <FiGitBranch className="h-4 w-4 group-hover:stroke-[2.2px]  dark:group-hover:stroke-blue-400 group-hover:stroke-red-500 " />
-        <p className="font-semibold text-sm group-hover:font-extrabold ">
-          {forks}
-        </p>
-      </div>
+      {gitHubInfo.map((info, id) => {
+        return (
+          <div
+            key={id}
+            className="flex dark:group-hover:text-blue-400 group-hover:text-red-500 space-x-1 items-center"
+          >
+            {
+              <info.icon className="h-4 w-4 group-hover:stroke-[2.2px] dark:group-hover:fill-blue-400 group-hover:fill-red-500" />
+            }
+            <p className="font-semibold text-sm group-hover:font-extrabold">
+              {info.label}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -47,6 +47,7 @@ const ProjectCard = forwardRef<HTMLDivElement, ProjectCardProps>(
                     src={project.image}
                     alt={project.title}
                     width="200"
+                    // TODO: Need to add a background color & the rest of the border color
                     className="rounded-md drop-shadow-2xl hover:scale-110 border-l-teal-400 border-2"
                   />
                 </Suspense>
